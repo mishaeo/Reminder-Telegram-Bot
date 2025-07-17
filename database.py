8ef1d991ca01520a2983cb0a312cd48bcebdd632
Mischa <150851766+mishaeo@users.noreply.github.com>
2025-07-17 09:53:59 +0200

    Creating a new users table (test_2)

--- database.py
+++ database.py
@@ -45,7 +45,8 @@ class Reminder(Base):
 
 async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.drop_all)    # удалит все таблицы
+        await conn.run_sync(Base.metadata.create_all)  # создаст заново с правильной схемой
 
 async def create_user_remind(telegram_id: int, title: str, reminder_time, message: str):
     if isinstance(reminder_time, str):
