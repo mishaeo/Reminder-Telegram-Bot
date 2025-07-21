6ffcfe7f1988b96a1a1f9ea715b94e54255f447d
Mischa <150851766+mishaeo@users.noreply.github.com>
2025-07-21 16:52:11 +0200

    The main buttons are divided into two rows.

--- keyboards.py
+++ keyboards.py
@@ -4,7 +4,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 remind_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text="Create", callback_data="create"),
-        InlineKeyboardButton(text="Delete", callback_data="delete"),
+        InlineKeyboardButton(text="Delete", callback_data="delete")
+    ],
+    [
         InlineKeyboardButton(text="Show", callback_data="show"),
         InlineKeyboardButton(text="Edit", callback_data="edit")
     ]
